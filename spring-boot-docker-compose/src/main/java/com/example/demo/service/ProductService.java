6c9e528c950094efe169eef838564457e59c5076
Jasna.J <Jasna.J@EXIND3010-143.allfon.in>
2022-08-12 18:32:29 +0530

    docker compose

---
+++ spring-boot-docker-compose/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.ifaces.ProductRepository;
+import com.example.demo.model.Product;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public ProductService(ProductRepository repo) {
+		super();
+		this.repo = repo;
+	}
+	public Product add(Product entity) {
+		
+		return this.repo.save(entity);
+	}
+
+
+
+	public List<Product> findAll() {
+
+		return this.repo.findAll();
+	}
+
+}
